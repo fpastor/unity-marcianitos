4a2039f35d580dc8bc5a59332a1d1443101e4b4d
Fernando Pastor <fpastor@icloud.com>
2024-09-15 01:12:21 +0200

    Enemy Explosion

--- Assets/Scripts/Disparo.cs
+++ Assets/Scripts/Disparo.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 public class Disparo : MonoBehaviour
 {
+    [SerializeField] Transform prefabExplosion;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y > 5)
+            Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Enemy")
         {
+            // Instanciamos la explosión.
+            Transform explosion = Instantiate(prefabExplosion, other.transform.position, Quaternion.identity);
             Destroy(other.gameObject);
+            Destroy(explosion.gameObject, 1f);
             Destroy(gameObject);
         }
     }
-
-
 }
